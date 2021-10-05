52248b603f766fd0d46da49de4f36a5893f81c3c
Pablo Vinicius <pvinicius006@hotmail.com>
2021-10-05 20:40:48 -0300

    update

--- Rest/Rest/urls.py
+++ Rest/Rest/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 from games.api import viewsets as gamesviewsets
 
@@ -26,4 +27,4 @@ route.register(r'games', gamesviewsets.GamesViewSet, basename="games")
 urlpatterns = [
     path('', include(route.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Rest/games/models.py
+++ Rest/games/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from uuid import uuid4
 
+def upload_image(instance, filename):
+    return f"{instance.id_game}-{filename}"
+
+
 # Create your models here.
 class Games(models.Model):
     id_game = models.UUIDField(primary_key=True, default=uuid4, editable=False)
@@ -9,3 +13,4 @@ class Games(models.Model):
     publisher = models.CharField(max_length=100)
     realease_date = models.IntegerField()
     create_at = models.DateField(auto_now_add=True)
+    image = models.ImageField(upload_to=upload_image, blank=True, null=True)
